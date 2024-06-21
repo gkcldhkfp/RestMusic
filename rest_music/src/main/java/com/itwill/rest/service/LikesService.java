a30573d60967f3748d6b4aa9f54f242e16dfce17
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-06-21 20:23:57 +0900

    라이크 컨트롤러, 라이크 서비스 수정

--- rest_music/src/main/java/com/itwill/rest/service/LikesService.java
+++ rest_music/src/main/java/com/itwill/rest/service/LikesService.java
@@ -2,11 +2,34 @@ package com.itwill.rest.service;
 
 import org.springframework.stereotype.Service;
 
+import com.itwill.rest.repository.Likes;
 import com.itwill.rest.repository.LikesDao;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class LikesService {
-	    private LikesDao chartDao;
-	    
-	    
+	private final LikesDao likesDao;
+
+	public int countLikes(Integer songId, Integer id) {
+		return likesDao.countLikes(songId, id);
+
+	}
+
+	public boolean insertLike(Integer songId, Integer id) {
+		Likes like = Likes.builder().songId(songId).id(id).build();
+		int result = likesDao.insertLikes(like);
+		return result > 0;
+
+	}
+
+	public boolean deleteLike(Integer songId, Integer id) {
+		int result = likesDao.deleteLikes(songId, id);
+		return result > 0;
+
+	}
+
 }
--- rest_music/src/main/java/com/itwill/rest/web/LikesController.java
+++ rest_music/src/main/java/com/itwill/rest/web/LikesController.java
@@ -1,5 +1,19 @@
 package com.itwill.rest.web;
 
-public class LikesController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/song")
+public class LikesController {
+	// localhost:8080/song/popular => top30
+	// localhost:8080/song/genre => 장르별 차트
+	// localhost:8080/song/video => 뮤직비디오 차트
+	
+	
 }
