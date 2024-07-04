a80095eebf1e0734c101419c0d5eff8ed70bc315
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-07-04 10:57:20 +0900

    Update (final)rest.sql
    songs 테이블에서 genre 컬럼 -> genre_id로 변경
    roles 테이블 이름을 -> artist_roles로 변경 후 role_id 컬럼 추가
    users 테이블에서 user_name 컬럼 유니크 속성 삭제
    playlist_songs 테이블에서 created_time 컬럼 추가
    
    새로 추가한 테이블들
    artists, artists_roles, code_master, role_code, genre_code

--- SQL/(final)rest.sql
+++ SQL/(final)rest.sql
@@ -1,10 +1,13 @@
 /*
- * 테이블 13개
- *      albums,     songs,  title_songs,    singers,    composers,  writers,  arrangers,
- *      users,     likes,   comments,  playlists,   playlist_songs,  pur_users           
+ * 테이블 15개 (컬럼 개수)
+ *      albums (5개),     songs (7개),  title_songs (2개),    artists (4개),    artist_roles (3개),
+ *      code_master (2개),    role_code (3개),  genre_code (3개), users (9개),     likes (2개),
+ *      album_likes (2개),    comments (6개),  playlists (3개),   playlist_songs (3개),  pur_users (2개)
  *
- * 고유키(PK) 5개
- *      album_id,   song_id,    id,     c_id,   p_list_id
+ * 고유키(PK) 9개
+ *      album_id (albums 테이블),   song_id (songs 테이블),   artist_id (artists 테이블), code_id (code_master 테이블),           
+ *      role_id (role_code 테이블),    genre_id (genre_code 테이블),  id (users 테이블),  
+ *      c_id (comments 테이블), p_list_id (playlists 테이블)
  */
 
 create table albums ( -- 앨범 (컬럼 5개)
@@ -25,10 +28,11 @@ create table songs ( -- 음악 (컬럼 7개)
         video_link                          varchar2(200), -- 뮤비 링크 (200바이트)
         
         ------- 장르 코드 테이블로 변경해야됨!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        genre                               varchar2(10 char) not null, -- 음악 장르 (10글자, 낫 널)
+        genre_id                            number(4), -- 장르 번호 (4자리)
         
         constraint songs_song_id_pk         primary key (song_id), -- 음악 번호 (고유키)
-        constraint songs_album_id_fk        foreign key (album_id) references albums (album_id) -- 앨범 번호 (외래키)
+        constraint songs_album_id_fk        foreign key (album_id) references albums (album_id), -- 앨범 번호 (외래키)
+        constraint songs_genre_code_id_fk   foreign key (genre_id) references genre_code (genre_id) -- 장르 번호 (외래키)
 
 );
 
@@ -40,71 +44,45 @@ create table title_songs ( -- 타이틀 곡 (컬럼 2개)
 
 );
 
---create table singers ( -- 가수 (컬럼 2개)
---        song_id                             number(5), -- 음악 번호 (5자리)
---        singer_name                         varchar2(20 char) not null, -- 가수 이름 (20글자, 낫 널)
---        constraint singers_song_id_fk       foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
---
---);
---
---create table composers ( -- 작곡가 (컬럼 2개)
---        song_id                             number(5), -- 음악 번호 (5자리)
---        composer                            varchar2(20 char) not null, -- 작곡가 (20글자, 낫 널)
---        constraint composers_song_id_fk     foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
---
---);
---
---create table writers ( -- 작사가 (컬럼 2개)
---        song_id                             number(5), -- 음악 번호 (5자리)
---        writer                              varchar2(20 char) not null, -- 작사가 (20글자, 낫 널)
---        constraint writers_song_id_fk       foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
---
---);
---
---create table arrangers ( -- 편곡자 (컬럼 2개)
---        song_id                             number(5), -- 음악 번호 (5자리)
---        arranger                            varchar2(20 char) not null, -- 편곡자 (20글자, 낫 널)
---        constraint arrangers_song_id_fk     foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
---
---);
-
-create table artists ( -- 아티스트 (컬럼 3개)
-        artist_id                       number(4) generated as identity, -- 아티스트 번호 (4자리, 자동 생성)
-        artist_name                     varchar2(20 char) not null, -- 아티스트 이름 (20글자, 낫 널)
-        artist_image                    varchar2(100), -- 아티스트 이미지 (100바이트)
-        artist_description              varchar2(100), -- 아티스트 설명(나무위키) 이 적혀져있는 txt파일 경로
-       constraint artists_artist_id_pk   primary key (artist_id) -- 아티스트 번호 (고유키)
+create table artists ( -- 아티스트 (컬럼 4개)
+        artist_id                           number(4) generated as identity, -- 아티스트 번호 (4자리, 자동 생성)
+        artist_name                         varchar2(20 char) not null, -- 아티스트 이름 (20글자, 낫 널)
+        artist_image                        varchar2(100), -- 아티스트 이미지 (100바이트)
+        artist_description                  varchar2(100), -- 아티스트 설명 (100바이트) (나무위키) 이 적혀져있는 txt파일 경로
+       constraint artists_artist_id_pk      primary key (artist_id) -- 아티스트 번호 (고유키)
         
-)
+);
 
-create table roles ( -- 아티스트 참여 음원 (컬럼 3개)
-    artist_id                           number(4), -- 아티스트 번호 (4자리)
-    song_id                             number(5), -- 음악 번호 (5자리)
-    -- role                                varchar2(20 char), -- 코드 네임 (20글자)
-    constraint roles_artist_id_fk       foreign key (artist_id) references artists (artist_id), -- 아티스트 번호 (외래키)
-    constraint roles_song_id_fk         foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
-    -- constraint roles_role_fk            foreign key (role) references code_master (code_name) -- 코드 네임 (외래키)
+create table artist_roles ( -- 아티스트 참여 음원 (컬럼 3개)
+    artist_id                                  number(4), -- 아티스트 번호 (4자리)
+    song_id                                    number(5), -- 음악 번호 (5자리)
+    role_id                                    number(4), -- 역할 번호 (4자리)
+    constraint artist_roles_artist_id_fk       foreign key (artist_id) references artists (artist_id), -- 아티스트 번호 (외래키)
+    constraint artist_roles_song_id_fk         foreign key (song_id) references songs (song_id), -- 음악 번호 (외래키)
+    constraint artist_roles_role_id_fk         foreign key (role) references role_code (role_id) -- 역할 번호 (외래키)
 )
 
 create table code_master ( -- 코드 마스터 (컬럼 2개)
-    code_id                                 number(4), -- 코드 번호 (4자리, 자동 생성)
+    code_id                                 number(4), -- 코드 번호 (4자리) (자동 생성 아님)
     code_name                               varchar2(20 char) not null, -- 코드 네임 (20글자, 낫 널)
     -- code_id: 1, code_name:roles
     -- code_id: 2, code_name:genre
-    constraint code_master_code_id_pk      primary key (code_id) -- 역할 번호 (고유키)
+    constraint code_master_code_id_pk       primary key (code_id) -- 역할 번호 (고유키)
 )
 
-create table role_code ( -- 역할 코드 (컬럼 2개)
-    code_id                                 ,--대충 forign key
+create table role_code ( -- 역할 코드 (컬럼 3개)
+    code_id                                 number(4), -- 코드 번호 (4자리)
     role_name                               varchar2(20 char) not null, -- 역할 이름 (20글자, 낫 널) ex: 가수, 작곡가, 작사가, 편곡가
-    role_id                                 number(4) generated as identity, -- 역할 번호 (4자리, 자동 생성)ex: 가수10, 작곡가20, 
+    role_id                                 number(4), -- 역할 번호 (4자리) (자동 생성 아님) ex: 가수 - 10, 작곡가 - 20, 작사가 - 30 ...
+    constraint role_code_code_id_fk         foreign key (code_id) references code_master (code_id), -- 코드 번호 (외래키)
     constraint role_code_role_id_pk         primary key (role_id) -- 역할 번호 (고유키)
 )
 
-create table genre_code ( -- 장르 코드 (컬럼 2개)
-    code_id                                 ,--대충 forign key
+create table genre_code ( -- 장르 코드 (컬럼 3개)
+    code_id                                 number(4), -- 코드 번호 (4자리)
     genre_name                              varchar2(10 char) not null, -- 장르 이름 (10글자, 낫 널) ex: 발라드, 팝, 힙합
-    genre_id                                number(4) generated as identity, -- 장르 번호 (4자리, 자동 생성)ex: 발라드10, 팝20, 힙합30
+    genre_id                                number(4), -- 장르 번호 (4자리) (자동 생성 아님) ex: 발라드 - 10, 팝 - 20, 힙합 - 30 ...
+    constraint genre_code_code_id_fk        foreign key (code_id) references code_master (code_id), -- 코드 번호 (외래키)
     constraint genre_code_genre_id_pk       primary key (genre_id) -- 장르 번호 (고유키)
 )
  
@@ -132,11 +110,11 @@ create table likes ( -- 좋아요 (컬럼 2개)
 
 );
 
-create table album_likes( -- 앨범 좋아요
-        album_id                            number(5), -- 음악 번호 (5자리)
-        id                                  number(4), -- 회원 번호 (4자리)
-        constraint likes_album_id_fk         foreign key (album_id) references albums (album_id), -- 음악 번호 (외래키)
-        constraint likes_id_fk              foreign key (id) references users (id) -- 회원 번호 (외래키)        
+create table album_likes( -- 앨범 좋아요 (컬럼 2개)
+        album_id                                number(4), -- 앨범 번호 (5자리)
+        id                                      number(4), -- 회원 번호 (4자리)
+        constraint album_likes_album_id_fk      foreign key (album_id) references albums (album_id), -- 앨범 번호 (외래키)
+        constraint album_likes_id_fk            foreign key (id) references users (id) -- 회원 번호 (외래키)        
 )
 
 create table comments ( -- 댓글 (컬럼 6개)
@@ -160,10 +138,10 @@ create table playlists ( -- 플레이리스트 (컬럼 3개)
 
 );
 
-create table playlist_songs ( -- 플레이리스트 저장 곡 (컬럼 2개)
+create table playlist_songs ( -- 플레이리스트 저장 곡 (컬럼 3개)
         p_list_id                               number(4), -- 플리 번호 (4자리)
         song_id                                 number(5), -- 음악 번호 (5자리)
-        created_time                            timestamp default systimestamp, -- 플리 저장 시간 (날짜 - 시분초 표시함)         
+        created_time                            timestamp default systimestamp, -- 플리 저장 시간 (현재 시간 - 시분초까지 표시함)         
         constraint playlist_songs_p_list_id_fk  foreign key (p_list_id) references playlists (p_list_id), -- 플리 번호 (외래키)
         constraint playlist_songs_song_id_fk    foreign key (song_id) references songs (song_id) -- 음악 번호 (외래키)
 
